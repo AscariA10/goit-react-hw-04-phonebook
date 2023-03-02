d44b0a8c9c6def81f414229bc2bf5087c60d09aa
VolodymyrKravchenko <109697703+AscariA10@users.noreply.github.com>
2023-03-02 18:12:55 +0200

    local storage functional added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,29 @@ import { Wrapper } from './App.styled';
 export class App extends Component {
    state = {
       contacts: [
-         { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-         { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-         { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-         { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+         // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+         // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+         // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+         // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
       ],
       filter: '',
    };
+
+   componentDidMount() {
+      const savedContactList = JSON.parse(localStorage.getItem('saved-contact-list'));
+
+      console.log(savedContactList);
+
+      if (!savedContactList) {
+         console.log('savedContactList', savedContactList);
+         return;
+      }
+      this.setState({ contacts: [...savedContactList] });
+   }
+
+   componentDidUpdate() {
+      localStorage.setItem('saved-contact-list', JSON.stringify(this.state.contacts));
+   }
 
    onFormSubmit = data => {
       if (this.contactCheck(data.name)) {
